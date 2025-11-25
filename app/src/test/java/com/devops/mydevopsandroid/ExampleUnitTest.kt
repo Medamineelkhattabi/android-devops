ecf8fa1e954ebe6ca48304743666a9916e060dc6
Medamineelkhattabi <medamine4elkhattabi@gmail.com>
2025-11-25 23:32:21 +0100

    Initial Android project

---
+++ app/src/test/java/com/devops/mydevopsandroid/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package com.devops.mydevopsandroid
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun substract_isCorrect() {
+        assertEquals(4, 8 - 4)
+    }
+}
